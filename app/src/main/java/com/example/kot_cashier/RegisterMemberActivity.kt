d88a365668e7ff441ab237eee51edb65776e3d76
DA-UN <ekdns070@naver.com>
2021-10-28 20:27:05 +0900

    FIREBASE

--- app/src/main/java/com/example/kot_cashier/RegisterMemberActivity.kt
+++ app/src/main/java/com/example/kot_cashier/RegisterMemberActivity.kt
@@ -1,24 +1,81 @@
 package com.example.kot_cashier
 
+import android.content.ContentValues.TAG
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.ktx.Firebase
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.activity_main.moveSignUpButton
 import kotlinx.android.synthetic.main.activity_registerdata.*
 
+private var backPressedTime : Long = 0
+data class userInfo(
+
+    var userName: String? = "",
+    var userPwd: String?="",
+    var userUID: String?="",
+    var userEmail: String?=""
+)
+
 class RegisterMemberActivity : AppCompatActivity() {
-    private var backPressedTime : Long = 0
+    var fbAuth : FirebaseAuth? =null
+    var fbFirestore : FirebaseFirestore? = null
+    private lateinit var auth: FirebaseAuth  //사용자
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_registerdata)
+        fbAuth = FirebaseAuth.getInstance() //파이어베이스 인스턴스 생성
+        fbFirestore = FirebaseFirestore.getInstance()
+        var uInfo = userInfo()
+
+        if(intent.hasExtra("email")){
+            uInfo.userEmail=intent.getStringExtra("email")
+            editTextEmail.text=uInfo.userEmail
+        }
+
 
         completeSignupButton.setOnClickListener({
-            val intent = Intent(this, ShowFinishSignUpActivity::class.java)
-            startActivity(intent)
+
+
+            //유저 데이터 구조에 정보 저장
+            uInfo.userName=editTextName.text.toString()
+            uInfo.userUID=editTextPersonNum1.text.toString() + editTextPersonNum2.text.toString()
+            uInfo.userPwd=editTextPassword.text.toString()
+
+
+            if(!uInfo.userName.isNullOrEmpty() && !editTextPersonNum1.text.toString().isNullOrEmpty() && !editTextPersonNum2.text.toString().isNullOrEmpty() && !uInfo.userPwd.isNullOrEmpty() && !uInfo.userEmail.isNullOrEmpty()){
+
+
+                auth = Firebase.auth
+                //이메일 비밀번호 활용하여 사용자 정보 저장
+                auth.createUserWithEmailAndPassword(uInfo.userEmail.toString(), uInfo.userPwd.toString())
+                    .addOnCompleteListener(this){task->
+                      if(task.isSuccessful){
+                        Log.d(TAG, "createUserWithEmail:success")
+                      }else{
+                          Log.d(TAG, "createUserWithEmail:fail")
+                      }
+                    }
+                fbFirestore?.collection("users")?.document(uInfo.userEmail.toString())?.set(uInfo)
+                //?.collection("users")?.document(auth.uid.toString())?.set(uInfo)
+                val intent = Intent(this, ShowFinishSignUpActivity::class.java)
+                startActivity(intent)
+
+
+            }
+            else{
+                Toast.makeText(this@RegisterMemberActivity,"회원 정보를 제대로 입력하세요!!",Toast.LENGTH_SHORT).show()
+            }
+
         })
     }
 
--- app/src/main/java/com/example/kot_cashier/SignUpActivity.kt
+++ app/src/main/java/com/example/kot_cashier/SignUpActivity.kt
@@ -1,19 +1,110 @@
 package com.example.kot_cashier
 
+import android.content.ContentValues.TAG
 import android.content.Intent
 import android.os.Bundle
+import android.provider.ContactsContract
+import android.util.Log
+import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.FirebaseException
+import com.google.firebase.FirebaseTooManyRequestsException
+import com.google.firebase.auth.*
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import kotlinx.android.synthetic.main.activity_registerdata.*
 import kotlinx.android.synthetic.main.activity_signup.*
+import java.util.concurrent.TimeUnit
 
 class SignUpActivity : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
+    private var storedVerificationId: String? = ""
+    private lateinit var resendToken: PhoneAuthProvider.ForceResendingToken
+    private lateinit var callbacks: PhoneAuthProvider.OnVerificationStateChangedCallbacks
+    private var success=false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_signup)
+        auth= Firebase.auth
+
+        callbacks = object : PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+
+            override fun onVerificationCompleted(credential: PhoneAuthCredential) {
+                // This callback will be invoked in two situations:
+                // 1 - Instant verification. In some cases the phone number can be instantly
+                //     verified without needing to send or enter a verification code.
+                // 2 - Auto-retrieval. On some devices Google Play services can automatically
+                //     detect the incoming verification SMS and perform verification without
+                //     user action.
+                Toast.makeText(this@SignUpActivity, "인증번호 전송 완료", Toast.LENGTH_SHORT).show()
+                Log.d(TAG, "onVerificationCompleted:$credential")
+                success=true
+            }
+
+            override fun onVerificationFailed(e: FirebaseException) {
+                // This callback is invoked in an invalid request for verification is made,
+                // for instance if the the phone number format is not valid.
+                Log.w(TAG, "onVerificationFailed", e)
+
+                if (e is FirebaseAuthInvalidCredentialsException) {
+                    // Invalid request
+                } else if (e is FirebaseTooManyRequestsException) {
+                    // The SMS quota for the project has been exceeded
+                }
+
+                // Show a message and update the UI
+            }
+
+
+            override fun onCodeSent(
+                verificationId: String,
+                token: PhoneAuthProvider.ForceResendingToken
+            ) {
+                // The SMS verification code has been sent to the provided phone number, we
+                // now need to ask the user to enter the code and then construct a credential
+                // by combining the code with a verification ID.
+                Log.d(TAG, "onCodeSent:$verificationId")
+
+                // Save verification ID and resending token so we can use them later
+                storedVerificationId = verificationId
+                resendToken = token
+            }
+        }
 
         NextSignButton.setOnClickListener({
             val intent = Intent(this, RegisterMemberActivity::class.java)
-            startActivity(intent)
+            intent.putExtra("email",editTextPhone.text.toString())
+        //    if(success) {
+                startActivity(intent)
+           // }
+          //  else{
+           //     Toast.makeText(this@SignUpActivity, "인증번호가 틀렸습니다.", Toast.LENGTH_SHORT).show()
+          //  }
+        })
+
+        PhoneAuthButton.setOnClickListener({
+            var PhoneNum="+82" + editTextPhone.text.toString()
+            startPhoneNumberVerification(PhoneNum)
+
         })
     }
+
+    private fun startPhoneNumberVerification(phoneNumber: String) {
+        // [START start_phone_auth]
+        val options = PhoneAuthOptions.newBuilder(auth)
+            .setPhoneNumber(phoneNumber)       // Phone number to verify
+            .setTimeout(60L, TimeUnit.SECONDS) // Timeout and unit
+            .setActivity(this)                 // Activity (for callback binding)
+            .setCallbacks(callbacks)          // OnVerificationStateChangedCallbacks
+            .build()
+        PhoneAuthProvider.verifyPhoneNumber(options)
+        // [END start_phone_auth]
+    }
+    private fun verifyPhoneNumberWithCode(verificationId: String?, code: String) {
+        // [START verify_with_code]
+        val credential = PhoneAuthProvider.getCredential(verificationId!!, code)
+        // [END verify_with_code]
+    }
+
 }
